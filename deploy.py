3dd9b4c2078c0641ea7fd120308a0d0b1af793b6
Marc-Antoine Loignon <developer@lognoz.org>
2018-11-28 21:40:14 -0500

    Deploy: Add function to git push on ref master

--- deploy.py
+++ deploy.py
@@ -5,18 +5,33 @@ import sys
 import subprocess
 
 def main():
-   if "TRAVIS" in os.environ:
+   if is_travis():
       set_git_config()
 
    for module in os.listdir('.'):
       if os.path.isdir(module) and os.path.isfile(module + '/package.py'):
          commit_change(module)
 
+   if is_travis():
+      git_push()
+
+
 def set_git_config():
    if 'NAME' in os.environ and 'EMAIL' in os.environ:
       os.system(
          'git config user.email ' + os.environ['EMAIL'] + ' && ' \
          'git config user.name ' + os.environ['NAME'])
+
+def is_travis():
+   return "TRAVIS" in os.environ
+
+def git_push():
+   repository = output('git remote get-url origin') \
+      .replace('https://', '') \
+      .replace('http://', '') \
+      .replace('git://', '')
+
+   os.system('git push https://${GITHUB_TOKEN}@%s HEAD:master' % repository)
 
 def commit_change(module):
    if output('git status %s --porcelain | sed s/^...//' % module):
