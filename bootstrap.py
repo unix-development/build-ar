6c199fe46e524e3d16a6ebd543167049f8ebf62f
Marc-Antoine Loignon <developer@lognoz.org>
2018-11-30 13:54:48 -0500

    Bootstrap: Remove makepkg argument in build_package function

--- bootstrap.py
+++ bootstrap.py
@@ -65,7 +65,7 @@ class Builder():
    def build_package(self):
       os.system(
          'sudo pacman -S $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]}) --noconfirm && ' \
-         'makepkg -Acs && ' \
+         'makepkg && ' \
          'mv *.pkg.tar.xz ../build-repository/');
 
    def clean_directory(self):
