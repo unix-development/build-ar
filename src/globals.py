4ce7708653035c6e2dd5ffa54342da17e8da07de
Marc-Antoine Loignon <developer@lognoz.org>
2019-01-23 13:29:38 -0500

    Global: Remove packages variable

--- src/globals.py
+++ src/globals.py
@@ -15,9 +15,6 @@ path_www = path_base + '/www'
 # Packages directory
 path_pkg = path_base + '/pkg'
 
-# Packages list
-packages = [ f for f in os.scandir(path_pkg) if f.is_dir() ]
-
 # Config list
 with open(path_base + '/repository.json') as file:
    config = json.load(file)
