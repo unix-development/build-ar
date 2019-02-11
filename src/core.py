d63d265117538485afcc33377d03d99929678168
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-11 10:08:38 -0500

    Core: Remove config variable

--- src/core.py
+++ src/core.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 import os
-import json
 
 # Base directory
-path_base = os.path.realpath(__file__).replace('/src/globals.py', '')
+path_base = os.path.realpath(__file__).replace('/src/core.py', '')
 
 # Mirror directory
 path_mirror = path_base + '/mirror'
@@ -14,7 +13,3 @@ path_www = path_base + '/www'
 
 # Packages directory
 path_pkg = path_base + '/pkg'
-
-# Config list
-with open(path_base + '/repository.json') as file:
-   config = json.load(file)
