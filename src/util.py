96c58c4f65536d795bdde2c0ef0f01951c9d4039
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-11 11:27:03 -0500

    Util: Add replace_ending and edit_file functions

--- src/util.py
+++ src/util.py
@@ -11,6 +11,16 @@ import fileinput
 def is_travis():
    return "TRAVIS" in os.environ
 
+def replace_ending(find, replace, string):
+   split = string.split(find, 1)
+   return split[0] + replace
+
+
+def edit_file(filename):
+   with fileinput.input(filename, inplace=1) as f:
+       for line in f:
+          yield line.rstrip('\n')
+
 def output(command):
    return subprocess.check_output(command, shell=True).decode(sys.stdout.encoding).strip()
 
