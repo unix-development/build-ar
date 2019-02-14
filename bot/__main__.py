a15eeb8e6706b76b4ee8d7a4b36aeb3494327b4f
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-14 16:59:36 -0500

    Main: Add main python file that will be uses as bot observator

---
+++ bot/__main__.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import json
+import validator
+
+from core import config
+from core import contextual
+
+contextual = contextual.init(
+   pwd = __file__
+)
+
+config = config.init(
+   path_base = contextual.path_base
+)
+
+validate = validator.init(
+   config = config,
+   contextual = contextual
+)
+
+if __name__ == "__main__":
+   if os.getuid() == 0:
+      sys.exit("This file needs to be not execute as root.")
+
+   validate.files()
+   validate.repository()
+   validate.ssh()
