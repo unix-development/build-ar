5dab4f97a8578d91445c2e1e02d245033108d6aa
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-15 01:21:01 -0500

    Core: Add function to get packages in contextual file

--- bot/core/contextual.py
+++ bot/core/contextual.py
@@ -4,14 +4,29 @@ import os
 
 class new():
    def __init__(self, **parameters):
+      self.parameters = parameters
+
+      self.setPathsLocation()
+      self.setPackages()
+
+   def setPackages(self):
+      packages = []
+      for name in os.listdir(self.path_pkg):
+         if os.path.isfile(self.path_pkg + '/' + name + '/package.py'):
+            packages.append(name)
+
+      packages.sort()
+      self.packages = packages
+
+   def setPathsLocation(self):
       # Base directory
-      self.path_base = os.path.realpath(parameters['pwd']).replace('bot/__main__.py', '')
+      self.path_base = os.path.realpath(self.parameters["pwd"]).replace("/bot/__main__.py", "")
 
       # Mirror directory
-      self.path_mirror = self.path_base + '/mirror/'
+      self.path_mirror = self.path_base + "/mirror"
 
       # Html directory
-      self.path_www = self.path_base + '/www/'
+      self.path_www = self.path_base + "/www"
 
       # Packages directory
-      self.path_pkg = self.path_base + '/pkg/'
+      self.path_pkg = self.path_base + "/pkg"
