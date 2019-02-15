9cb914990201cde48cd24a58f9150b1ee1df57d3
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-15 11:49:19 -0500

    Packages: Add repository class that add packages into database

--- bot/packages.py
+++ bot/packages.py
@@ -23,6 +23,39 @@ class new(constructor):
          module.pull()
          module.make()
 
+      repository(
+         config = self.config,
+         database = self.config("database"),
+         path_mirror = self.path_mirror
+      )
+
+class repository(constructor):
+   def construct(self):
+      os.chdir(self.path_mirror)
+
+      self.add_packages()
+      self.clean_directory()
+
+   def clean_directory(self):
+      scripts = [
+         "rm -f ./%s.old" % self.database,
+         "rm -f ./%s.files" % self.database,
+         "rm -f ./%s.files.tar.gz" % self.database,
+         "rm -f ./%s.files.tar.gz.old" % self.database
+      ]
+
+      for script in scripts:
+         os.system(script)
+
+   def add_packages(self):
+      scripts = [
+         "rm -f ./%s.db" % self.database,
+         "rm -f ./%s.db.tar.gz" % self.database,
+         "repo-add ./%s.db.tar.gz ./*.pkg.tar.xz" % self.database
+      ]
+
+      for script in scripts:
+         os.system(script)
 
 class package(constructor):
    def construct(self):
