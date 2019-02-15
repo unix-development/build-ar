c4a94b1fe589aed74f8118d4191d5a4742d2290e
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-14 23:23:27 -0500

    Main: Change config reference for config.get

--- bot/__main__.py
+++ bot/__main__.py
@@ -18,20 +18,11 @@ config = config.init(
 )
 
 validate = validator.init(
-   config = config,
-   contextual = contextual
+   config = config.get,
+   path_base = contextual.path_base
 )
 
 environment = environment.init(
-   config = config,
-   contextual = contextual
+   config = config.get,
+   path_base = contextual.path_base
 )
-
-if __name__ == "__main__":
-   if os.getuid() == 0:
-      sys.exit("This file needs to be not execute as root.")
-
-   validate.files()
-   validate.repository()
-   environment.prepare_ssh()
-   validate.ssh()
