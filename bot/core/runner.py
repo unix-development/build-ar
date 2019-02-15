6373a65e63845d554fcb7e5a4be7dec7403535ce
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-15 01:47:52 -0500

    Core: Add initial runner.py

---
+++ bot/core/runner.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+
+class new():
+   def __init__(self, **commands):
+      self.commands = commands
+
+      for index, arg in enumerate(sys.argv):
+         for name in commands:
+            if name == arg:
+               self.execute(name)
+               break;
+
+   def execute(self, name):
+      for function in self.commands[name]:
+         function()
