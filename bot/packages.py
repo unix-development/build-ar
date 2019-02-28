c6264ca4bed3f33810b14839b829820e5db93175
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-28 15:46:21 -0500

    Packages: Add aur detection

--- bot/packages.py
+++ bot/packages.py
@@ -1,102 +1,162 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 import os
+import re
 import sys
 import shutil
+import subprocess
 
 from utils.editor import edit_file, replace_ending, extract
 from utils.terminal import output
 from utils.constructor import constructor
 
+builder = []
+
+def new_package(name, self):
+    return package(
+       name = name,
+       packages = self.packages,
+       path_pkg = self.path_pkg,
+       path_mirror = self.path_mirror
+    )
+
 class new(constructor):
-   def build(self):
-      sys.path.append(self.path_pkg)
+    def build(self):
+        sys.path.append(self.path_pkg)
 
-      for name in self.packages:
-         module = package(
-            name = name,
-            path_pkg = self.path_pkg,
-            path_mirror = self.path_mirror
-         )
+        for name in self.packages:
+            if name not in builder:
+                module = new_package(name, self)
 
-         module.prepare()
-         module.pull()
-         module.make()
-
-         if module.compiled == True:
-            return
+             #if module.compiled == True:
+             #   return
 
 class package(constructor):
-   compiled = False
+    compiled = False
 
-   def construct(self):
-      __import__(self.name + ".package")
-      os.chdir(self.path_pkg + "/" + self.name)
+    def construct(self):
+        __import__(self.name + ".package")
 
-      self.path = self.path_pkg + "/" + self.name
-      self.package = sys.modules[self.name + ".package"]
+        self.path = self.path_pkg + "/" + self.name
+        self.package = sys.modules[self.name + ".package"]
 
-   def prepare(self):
-      self.set_utils()
-      self.clean_directory()
+        self.add_to_builder()
+        self.separator()
+        self.chdir()
+        self.prepare()
+        self.pull()
+        self.make()
 
-   def make(self):
-      self.version = extract(self.path, "pkgver")
+    def add_to_builder(self):
+        builder.append(self.name)
 
-      if "pre_build" in dir(self.package):
-         self.package.pre_build()
+    def chdir(self):
+        os.chdir(self.path_pkg + "/" + self.name)
 
-      if not self.is_builded():
-         self.compiled = True
-         self.commit()
-         self.install_dependencies()
-         self.build()
+    def separator(self):
+        line = "── %s " % self.name
+        line += "─" * (55 - len(self.name))
 
-   def commit(self):
-      os.system(
-         'git add . && ' + \
-         'git commit -m "Bot: Add last update into ' + self.package.name + ' package ~ version ' + self.version + '"')
+        print("\n%s" % line)
 
-   def is_builded(self):
-      if not output("git status . --porcelain | sed s/^...//"):
-         return False
+    def prepare(self):
+        self.set_utils()
+        self.clean_directory()
 
-      for f in os.listdir(self.path_mirror):
-         if f.startswith(self.name + '-' + self.version + '-'):
+    def read(self):
+        command = 'echo $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]})'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        line = process.stdout.readlines()[0]
+
+        return line.strip().decode("UTF-8").split(" ")
+
+    def make(self):
+        self.version = extract(self.path, "pkgver")
+
+        if "pre_build" in dir(self.package):
+            self.package.pre_build()
+
+        if self.has_new_version():
+            self.verify_dependencies()
+            self.build()
+
+        #if not self.is_builded():
+        #   self.compiled = True
+        #   self.commit()
+        #   self.install_dependencies()
+
+    def commit(self):
+        os.system(
+            "git add . && " + \
+            "git commit -m \"Bot: Add last update into " + self.package.name + " package ~ version " + self.version + "\"")
+
+    def has_new_version(self):
+
+        if output("git status . --porcelain | sed s/^...//") is None:
             return False
 
-      return True
+        for f in os.listdir(self.path_mirror):
+            if f.startswith(self.name + '-' + self.version + '-'):
+                return False
 
-   def install_dependencies(self):
-      packages = output("source ./PKGBUILD && echo ${makedepends[@]}")
+        return True
 
-      if packages.strip() != "":
-         os.system("sudo pacman -S %s --noconfirm" % packages)
+    def verify_dependencies(self):
+        redirect = False
+        official = []
+        aur = []
 
-   def build(self):
-      os.system(
-         "makepkg -Asc --skipinteg && " \
-         "mv *.pkg.tar.xz " + self.path_mirror);
+        for dependency in self.read():
+            try:
+                output("pacman -Ssq %s | grep '^%s$'" % (dependency, dependency))
+                official.append(dependency)
+            except:
+                aur.append(dependency)
+                pass
 
-   def pull(self):
-      os.system(
-         "git init --quiet && " \
-         "git remote add origin " + self.package.source + " && " \
-         "git pull origin master --quiet && " \
-         "rm -rf .git")
+        for name in aur:
+            if name not in self.packages:
+                exit("exeption")
 
-      if os.path.isfile(".SRCINFO"):
-         os.remove(".SRCINFO")
+            redirect = True
+            module = package(
+                name = name,
+                packages = self.packages,
+                path_pkg = self.path_pkg,
+                path_mirror = self.path_mirror
+            )
 
-   def clean_directory(self):
-      files = os.listdir(".")
-      for f in files:
-         if os.path.isdir(f):
-            shutil.rmtree(f)
-         elif os.path.isfile(f) and f != "package.py":
-            os.remove(f)
+        if redirect == True:
+            self.separator()
 
-   def set_utils(self):
-      self.package.edit_file = edit_file
-      self.package.replace_ending = replace_ending
+        os.chdir(self.path_pkg + "/" + self.name)
 
+    def build(self):
+        os.system(
+            "makepkg --clean --syncdeps --skipinteg --noconfirm --nocheck --install && " \
+            "mv *.pkg.tar.xz " + self.path_mirror);
+
+    def pull(self):
+        print("Clone repository:")
+
+        os.system(
+            "git init --quiet; "
+            "git remote add origin " + self.package.source + "; "
+            "git pull origin master; "
+            "rm -rf .git;")
+
+        if os.path.isfile(".SRCINFO"):
+            os.remove(".SRCINFO")
+
+    def clean_directory(self):
+        files = os.listdir(".")
+        for f in files:
+            if os.path.isdir(f):
+                os.system("rm -rf " + f)
+            elif os.path.isfile(f) and f != "package.py":
+                os.remove(f)
+
+    def set_utils(self):
+        self.package.edit_file = edit_file
+        self.package.replace_ending = replace_ending
