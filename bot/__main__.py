c85166618a9a3b140083ab51529bf35ff697564c
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-28 11:34:48 -0500

    Main: Remove unexpected spaces around keyword

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-import os
-import sys
-import json
 import mirror
 import packages
 import interface
@@ -14,56 +11,66 @@ from core import config
 from core import contextual
 
 contextual = contextual.new(
-   pwd = __file__
+    pwd=__file__
 )
 
 config = config.new(
-   path_base = contextual.path_base
+    path_base=contextual.path_base
 )
 
 validate = validator.new(
-   config = config.get,
-   path_base = contextual.path_base
+    config=config.get,
+    path_base=contextual.path_base,
+    path_pkg=contextual.path_pkg,
+    path_mirror=contextual.path_mirror,
+    packages=contextual.packages
 )
 
 environment = environment.new(
-   config = config.get,
-   path_base = contextual.path_base
+    config=config.get,
+    path_base=contextual.path_base
 )
 
 interface = interface.new(
-   config = config.get,
-   packages = contextual.packages,
-   path_pkg = contextual.path_pkg,
-   path_www = contextual.path_www,
-   path_mirror = contextual.path_mirror
+   config=config.get,
+   packages=contextual.packages,
+   path_pkg=contextual.path_pkg,
+   path_www=contextual.path_www,
+   path_mirror=contextual.path_mirror
 )
 
 packages = packages.new(
-   config = config.get,
-   packages = contextual.packages,
-   path_pkg = contextual.path_pkg,
-   path_mirror = contextual.path_mirror
+   config=config.get,
+   packages=contextual.packages,
+   path_pkg=contextual.path_pkg,
+   path_mirror=contextual.path_mirror
 )
 
 mirror = mirror.new(
-   config = config.get,
-   is_travis = environment.is_travis,
-   path_mirror = contextual.path_mirror
+   config=config.get,
+   is_travis=environment.is_travis,
+   path_mirror=contextual.path_mirror
 )
 
 runner.new(
-   validate = [
-      validate.files,
-      validate.repository,
-      environment.prepare_ssh,
-      validate.ssh
-   ],
-   build = [
+   validate=[
+      validate.requirements,
       validate.files,
       validate.repository,
       environment.prepare_ssh,
       validate.ssh,
+      validate.container
+   ],
+   build=[
+      validate.requirements,
+      validate.files,
+      validate.travis,
+      validate.repository,
+      environment.prepare_ssh,
+      validate.ssh,
+      validate.container,
+      environment.prepare_git,
+      environment.prepare_pacman,
       packages.build,
       mirror.build,
       interface.build,
