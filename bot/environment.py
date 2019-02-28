065d0a22557330a7f95ba442094f2525825bb047
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-28 13:22:20 -0500

    Environment: Fix indentation is not a multiple of four

--- bot/environment.py
+++ bot/environment.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 import os
 from utils.constructor import constructor
 
 class new(constructor):
-   def execute(self, scripts):
-      os.system("(" + scripts + ") &>/dev/null")
+    def execute(self, scripts):
+        os.system("(" + scripts + ") &>/dev/null")
 
-   def is_travis(self):
-      return "TRAVIS" in os.environ
+    def is_travis(self):
+        return "TRAVIS" in os.environ
 
-   def prepare_git(self):
-      email = self.config("git.email")
-      name = self.config("git.name")
+    def prepare_git(self):
+        email = self.config("git.email")
+        name = self.config("git.name")
 
-      self.execute(
-         "git config user.email '%s'; " % email +
-         "git config user.name '%s';" % name
-      )
+        self.execute(
+            "git config user.email '%s'; " % email +
+            "git config user.name '%s';" % name
+        )
 
-   def prepare_ssh(self):
-      host = self.config("ssh.host")
+    def prepare_ssh(self):
+        host = self.config("ssh.host")
 
-      self.execute(
-         "eval $(ssh-agent); " +
-         "chmod 600 %s/deploy_key; " % self.path_base +
-         "ssh-add -lf %s/deploy_key; " % self.path_base +
-         "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; " % host
-      )
+        self.execute(
+            "eval $(ssh-agent); " +
+            "chmod 600 %s/deploy_key; " % self.path_base +
+            "ssh-add -lf %s/deploy_key; " % self.path_base +
+            "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; " % host
+        )
 
-   def prepare_pacman(self):
-      database = self.config("database")
+    def prepare_pacman(self):
+        database = self.config("database")
 
-      if os.path.exists(self.path_base + "/mirror/" + database + ".db") == False:
-         return
+        if os.path.exists(self.path_base + "/mirror/" + database + ".db") == False:
+            return
 
-      with open("/etc/pacman.conf", "r+") as file:
-         for line in file:
-            if line.strip() == "[%s]" % database:
-               break
-         else:
-            content = [
-               "[%s]" % database,
-               "SigLevel = Optional TrustedOnly",
-               "Server = file:///%s/mirror" % self.path_base
-            ]
+        with open("/etc/pacman.conf", "r+") as file:
+            for line in file:
+                if line.strip() == "[%s]" % database:
+                   break
+                else:
+                    content = [
+                        "[%s]" % database,
+                        "SigLevel = Optional TrustedOnly",
+                        "Server = file:///%s/mirror" % self.path_base
+                    ]
 
-            file.write("\n".join(content))
+                    file.write("\n".join(content))
 
-      self.execute("sudo pacman -Sy --noconfirm")
+            self.execute("sudo pacman -Sy --noconfirm")
