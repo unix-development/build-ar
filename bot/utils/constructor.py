15766b2ed59b6412c22e22e5b9ff8cbaf7804217
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-28 13:02:08 -0500

    Utils: Add fluent function tools in constructor file

--- bot/utils/constructor.py
+++ bot/utils/constructor.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
-class constructor():
-   def __init__(self, **parameters):
-      for name in parameters:
-         setattr(self, name, parameters[name])
+import functools
 
-      if "construct" in dir(self):
-         self.construct()
+class constructor():
+    def __init__(self, **parameters):
+        for name in parameters:
+            setattr(self, name, parameters[name])
+
+        if "construct" in dir(self):
+            self.construct()
+
+def fluent(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        self = args[0]
+        func(*args, **kwargs)
+        return self
+
+    return wrapped
