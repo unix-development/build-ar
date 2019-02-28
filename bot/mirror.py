c270fa854fee18b556a39729d67b7038d359810d
Marc-Antoine Loignon <developer@lognoz.org>
2019-02-28 13:25:04 -0500

    Mirror: Fix indentation is not a multiple of four

--- bot/mirror.py
+++ bot/mirror.py
@@ -5,47 +5,47 @@ from utils.git import git_remote_path
 from utils.constructor import constructor
 
 class new(constructor):
-   def construct(self):
-      self.database = self.config("database")
+    def construct(self):
+        self.database = self.config("database")
 
-      for name in [ "port", "user", "host", "path" ]:
-         setattr(self, name, self.config("ssh." + name))
+        for name in [ "port", "user", "host", "path" ]:
+            setattr(self, name, self.config("ssh." + name))
 
-   def build(self):
-      os.chdir(self.path_mirror)
+    def build(self):
+        os.chdir(self.path_mirror)
 
-      self.create_database()
-      self.clean_directory()
+        self.create_database()
+        self.clean_directory()
 
-   def clean_directory(self):
-      scripts = [
-         "rm -f ./%s.old" % self.database,
-         "rm -f ./%s.files" % self.database,
-         "rm -f ./%s.files.tar.gz" % self.database,
-         "rm -f ./%s.files.tar.gz.old" % self.database
-      ]
+    def clean_directory(self):
+        scripts = [
+            "rm -f ./%s.old" % self.database,
+            "rm -f ./%s.files" % self.database,
+            "rm -f ./%s.files.tar.gz" % self.database,
+            "rm -f ./%s.files.tar.gz.old" % self.database
+        ]
 
-      for script in scripts:
-         os.system(script)
+        for script in scripts:
+            os.system(script)
 
-   def create_database(self):
-      scripts = [
-         "rm -f ./%s.db" % self.database,
-         "rm -f ./%s.db.tar.gz" % self.database,
-         "repo-add ./%s.db.tar.gz ./*.pkg.tar.xz" % self.database
-      ]
+    def create_database(self):
+        scripts = [
+            "rm -f ./%s.db" % self.database,
+            "rm -f ./%s.db.tar.gz" % self.database,
+            "repo-add ./%s.db.tar.gz ./*.pkg.tar.xz" % self.database
+        ]
 
-      for script in scripts:
-         os.system(script)
+        for script in scripts:
+            os.system(script)
 
-   def deploy(self):
-      ssh = "ssh -p " + self.port
-      location = self.user + "@" + self.host + ":" + self.path
+    def deploy(self):
+        ssh = "ssh -p " + self.port
+        location = self.user + "@" + self.host + ":" + self.path
 
-      os.system(
-         "rsync -avz --update --copy-links --progress -e '%s' %s/ %s"
-         % (ssh, self.path_mirror, location)
-      )
+        os.system(
+            "rsync -avz --update --copy-links --progress -e '%s' %s/ %s"
+            % (ssh, self.path_mirror, location)
+        )
 
-      if self.is_travis():
-         os.system('git push https://${GITHUB_TOKEN}@%s HEAD:master' % git_remote_path())
+        if self.is_travis():
+            os.system('git push https://${GITHUB_TOKEN}@%s HEAD:master' % git_remote_path())
