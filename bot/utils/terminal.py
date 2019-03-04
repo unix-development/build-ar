2f2f88e2df896878e75b12cb20a501394f5562a8
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 08:34:39 -0500

    Utils: Add column to terminal file

--- bot/utils/terminal.py
+++ bot/utils/terminal.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 import sys
 import subprocess
 
 def output(command):
     return subprocess.check_output(command, shell=True).decode(sys.stdout.encoding).strip()
+
+def column():
+    return int(output("tput cols")) - 4
