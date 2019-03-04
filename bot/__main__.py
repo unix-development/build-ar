c0ec29ceef21c8be893d95112eb6538656833c02
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 14:16:07 -0500

    Main: Use container to simplify the app

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,80 +1,42 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
-import mirror
-import packages
-import interface
-import validator
-import environment
+from core.container import container
+from core.runner import runner
 
-from core import runner
-from core import config
-from core import contextual
+container.bootstrap([
+    "core.contextual",
+    "environment",
+    "validator",
+    "interface",
+    "repository"
+])
 
-contextual = contextual.new(
-    pwd=__file__
-)
+runner.set("validate", [
+    "validator.requirements",
+    "validator.files",
+    "validator.travis",
+    "validator.repository",
+    "environment.prepare_ssh",
+    "validator.connection",
+    "validator.content"
+])
 
-config = config.new(
-    path_base=contextual.path_base
-)
+runner.set("build", [
+    "validator.requirements",
+    "validator.files",
+    "validator.travis",
+    "validator.repository",
+    "environment.prepare_ssh",
+    "validator.connection",
+    "validator.content",
+    "environment.prepare_git",
+    "environment.prepare_pacman",
+    "repository.synchronize",
+    "repository.create_database",
+    "interface.build",
+    "repository.deploy"
+])
 
-validate = validator.new(
-    config=config.get,
-    path_base=contextual.path_base,
-    path_pkg=contextual.path_pkg,
-    path_mirror=contextual.path_mirror,
-    packages=contextual.packages
-)
-
-environment = environment.new(
-    config=config.get,
-    path_base=contextual.path_base
-)
-
-interface = interface.new(
-    config=config.get,
-    packages=contextual.packages,
-    path_pkg=contextual.path_pkg,
-    path_www=contextual.path_www,
-    path_mirror=contextual.path_mirror
-)
-
-packages = packages.new(
-    config=config.get,
-    is_travis=environment.is_travis,
-    packages=contextual.packages,
-    path_mirror=contextual.path_mirror,
-    path_pkg=contextual.path_pkg
-)
-
-mirror = mirror.new(
-    config=config.get,
-    is_travis=environment.is_travis,
-    path_mirror=contextual.path_mirror
-)
-
-runner.new(
-    validate=[
-        validate.requirements,
-        validate.files,
-        validate.repository,
-        environment.prepare_ssh,
-        validate.connection,
-        validate.container
-    ],
-    build=[
-        validate.requirements,
-        validate.files,
-        validate.travis,
-        validate.repository,
-        environment.prepare_ssh,
-        validate.connection,
-        validate.container,
-        environment.prepare_git,
-        environment.prepare_pacman,
-        packages.build,
-        mirror.build,
-        interface.build,
-        mirror.deploy
-    ]
-)
+container.register("runner", runner)
+container.run()
