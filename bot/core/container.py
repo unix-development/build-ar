671b4f16bc2d079b397b3ebba712bf7e9a9e7f93
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 13:36:00 -0500

    Core: Add container file

---
+++ bot/core/container.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+
+class Container(object):
+    instances = dict()
+
+    def register(self, abstract, instance):
+        self.instances[abstract] = instance
+
+    def run(self, functions):
+        for name in functions:
+            self.get(name)()
+
+    def get(self, abstract):
+        return self.instances[abstract]
+
+    def bootstrap(self, bootstrappers):
+        for bootstrap in bootstrappers:
+            __import__(bootstrap)
+
+            module = sys.modules[bootstrap]
+            module.app = app
+            module.path = path
+            module.repo = repo
+
+            module.register(self)
+
+def app(abstract):
+    return container.get(abstract)
+
+def path(abstract):
+    return container.get("path." + abstract)
+
+def repo(abstract):
+    value = container.get("repository")
+    for key in abstract.split('.'):
+        value = value[key]
+
+    return value
+
+container = Container()
