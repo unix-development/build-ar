6076b428a7fc94062e7034e3dbf8c72a4e6546fd
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 20:26:37 -0500

    Interface: Add register function to add to container

--- bot/interface.py
+++ bot/interface.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 import os
 import time
@@ -8,70 +9,75 @@ from utils.editor import edit_file, extract
 from utils.interface import get_compressed_file, get_base64
 from utils.constructor import constructor
 
-class new(constructor):
+def register(container):
+    container.register("interface.build", build)
+
+def build():
+    path_mirror = path("mirror")
+    path_pkg = path("pkg")
+
     table = ""
-
-    content = \
-        "<tr>" + \
-            "<td><a href=\"$path\">$name</a></td>" + \
-            "<td>$version</td>" + \
-            "<td>$date</td>" + \
-            "<td>$description</td>" + \
+    content = (
+        "<tr>"
+            "<td><a href=\"$path\">$name</a></td>"
+            "<td>$version</td>"
+            "<td>$date</td>"
+            "<td>$description</td>"
         "</tr>"
+    )
 
-    def build(self):
-        for package in self.packages:
-            module = self.path_pkg + "/" + package
+    for package in app("packages"):
+        module = path_pkg + "/" + package
 
-            try:
-                file = open(module + "/PKGBUILD")
-            except FileNotFoundError:
-                continue
+        try:
+            open(module + "/PKGBUILD")
+        except FileNotFoundError:
+            continue
 
-            description = extract(module, "pkgdesc")
-            version = extract(module, "pkgver")
-            name = extract(module, "pkgname")
+        description = extract(module, "pkgdesc")
+        version = extract(module, "pkgver")
+        name = extract(module, "pkgname")
 
-            for path in os.listdir(self.path_mirror):
-                if path.startswith(package + "-" + version + "-"):
-                    content = self.content
-                    date = time.strftime("%d %h %Y",
-                        time.gmtime(os.path.getmtime(self.path_mirror + "/" + path)))
+        for location in os.listdir(path_mirror):
+            if location.startswith(package + "-" + version + "-"):
+                date = time.strftime("%d %h %Y",
+                    time.gmtime(os.path.getmtime(path_mirror + "/" + location)))
 
-                    self.table += (content
-                        .replace("$path", path)
-                        .replace("$name", package)
-                        .replace("$date", date)
-                        .replace("$version", version)
-                        .replace("$description", description))
+                table += (content
+                    .replace("$path", location)
+                    .replace("$name", package)
+                    .replace("$date", date)
+                    .replace("$version", version)
+                    .replace("$description", description))
 
-        self.move_to_mirror()
-        self.replace_variables()
-        self.compress()
+    move_to_mirror()
+    replace_variables(table)
+    compress()
 
-    def move_to_mirror(self):
-        os.system("cp " + self.path_www + "/index.html " + self.path_mirror)
 
-    def replace_variables(self):
-        remote_path = 'https://' + git_remote_path().rstrip('.git')
+def move_to_mirror():
+    os.system("cp " + path("www") + "/index.html " + path("mirror"))
 
-        for line in edit_file(self.path_mirror + "/index.html"):
-            line = (line
-                .replace("$content", self.table)
-                .replace("$path", self.config("url"))
-                .replace("$database", self.config("database"))
-                .replace("$remote_path", remote_path)
-                .replace("images/logo.png", "data:image/png;base64," + get_base64(self.path_www + "/images/logo.png")))
+def replace_variables(table):
+    remote_path = 'https://' + git_remote_path().rstrip('.git')
 
-            if line.strip() == "<link rel=\"stylesheet\" href=\"css/main.css\">":
-                line = "<style type=\"text/css\">"
-                line += get_compressed_file(self.path_www + "/css/main.css")
-                line += "</style>"
+    for line in edit_file(path("mirror") + "/index.html"):
+        line = (line
+            .replace("$content", table)
+            .replace("$path", repo("url"))
+            .replace("$database", repo("database"))
+            .replace("$remote_path", remote_path)
+            .replace("images/logo.png", "data:image/png;base64," + get_base64(path("www") + "/images/logo.png")))
 
-            print(line)
+        if line.strip() == "<link rel=\"stylesheet\" href=\"css/main.css\">":
+            line = "<style type=\"text/css\">"
+            line += get_compressed_file(path("www") + "/css/main.css")
+            line += "</style>"
 
-    def compress(self):
-        content = get_compressed_file(self.path_mirror + "/index.html")
+        print(line)
 
-        with open(self.path_mirror + "/index.html", "w") as f:
-            f.write(content)
+def compress():
+    content = get_compressed_file(path("mirror") + "/index.html")
+
+    with open(path("mirror") + "/index.html", "w") as f:
+        f.write(content)
