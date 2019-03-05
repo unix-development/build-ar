fced75063c89b9c5479359ee8fe50146527e21e7
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 20:10:19 -0500

    Environment: Optimize and define functions outside the class

--- bot/environment.py
+++ bot/environment.py
@@ -2,51 +2,56 @@
 # -*- coding:utf-8 -*-
 
 import os
-from utils.constructor import constructor
 
-class new(constructor):
-    def execute(self, scripts):
-        os.system("(" + scripts + ") &>/dev/null")
+def register(container):
+    container.register("environment.prepare_git", prepare_git)
+    container.register("environment.prepare_mirror", prepare_mirror)
+    container.register("environment.prepare_pacman", prepare_pacman)
+    container.register("environment.prepare_ssh", prepare_ssh)
 
-    def is_travis(self):
-        return "TRAVIS" in os.environ and os.environ["TRAVIS"] is not ""
+def execute(scripts):
+    os.system("(" + scripts + ") &>/dev/null")
 
-    def prepare_git(self):
-        email = self.config("git.email")
-        name = self.config("git.name")
+def prepare_mirror():
+    execute("chmod 777 %s/mirror" % path("base"))
 
-        self.execute(
-            "git config user.email '%s'; " % email +
-            "git config user.name '%s';" % name
-        )
+def prepare_git():
+    email = repo("git.email")
+    name = repo("git.name")
 
-    def prepare_ssh(self):
-        host = self.config("ssh.host")
+    execute(
+        "git config user.email '%s'; " % email +
+        "git config user.name '%s';" % name)
 
-        self.execute(
-            "eval $(ssh-agent); " +
-            "chmod 600 %s/deploy_key; " % self.path_base +
-            "ssh-add %s/deploy_key; " % self.path_base +
-            "mkdir -p ~/.ssh; " +
-            "chmod 0700 ~/.ssh; " +
-            "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; " % host
-        )
+def prepare_ssh():
+    host = repo("ssh.host")
+    path_base = path("base")
 
-    def prepare_pacman(self):
-        database = self.config("database")
+    execute(
+        "eval $(ssh-agent); " +
+        "chmod 600 %s/deploy_key; " % path_base +
+        "ssh-add %s/deploy_key; " % path_base +
+        "mkdir -p ~/.ssh; " +
+        "chmod 0700 ~/.ssh; " +
+        "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; " % host
+    )
 
-        if os.path.exists(self.path_base + "/mirror/" + database + ".db"):
-            with open("/etc/pacman.conf", "r+") as file:
-                for line in file:
-                    if line.strip() == "[%s]" % database:
-                       break
-                    else:
-                        content = [
-                            "[%s]" % database,
-                            "SigLevel = Optional TrustedOnly",
-                            "Server = file:///%s/mirror" % self.path_base
-                        ]
+def prepare_pacman():
+    database = repo("database")
+    path_base = path("base")
 
-                        file.write("\n".join(content))
+    if os.path.exists(path_base + "/mirror/" + database + ".db"):
+        with open("/etc/pacman.conf", "r+") as file:
+            for line in file:
+                if line.strip() == "[%s]" % database:
+                   break
+                else:
+                    content = [
+                        "[%s]" % database,
+                        "SigLevel = Optional TrustedOnly",
+                        "Server = file:///%s/mirror" % path_base
+                    ]
 
-        self.execute("sudo pacman -Sy --noconfirm")
+                    file.write("\n".join(content))
+
+    execute("sudo pacman -Sy --noconfirm")
