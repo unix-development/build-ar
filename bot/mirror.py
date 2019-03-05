c8b1d9fae1aeb22df89c00f119902028c3140d5e
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 20:12:33 -0500

    Mirror: Change the coding standards to use container

--- bot/mirror.py
+++ bot/mirror.py
@@ -1,51 +1,198 @@
 #!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
 import os
+import re
+import sys
+import shutil
+import subprocess
+
 from utils.git import git_remote_path
+from utils.editor import edit_file, replace_ending, extract
+from utils.terminal import output, title, bold
 from utils.constructor import constructor
 
-class new(constructor):
-    def construct(self):
-        self.database = self.config("database")
+builder = []
 
-        for name in [ "port", "user", "host", "path" ]:
-            setattr(self, name, self.config("ssh." + name))
+def register(container):
+    container.register("repository.synchronize", synchronize)
+    container.register("repository.create_database", create_database)
+    container.register("repository.deploy", deploy)
 
-    def build(self):
-        os.chdir(self.path_mirror)
+def synchronize():
+    sys.path.append(path("pkg"))
 
-        self.create_database()
+    for name in app("packages"):
+        if name not in builder:
+            if execute(name) is True and app("is_travis") is True:
+                return
+
+def deploy():
+    print(title("Deploy to host remote") + "\n")
+
+    os.chdir(path("base"))
+
+    os.system(
+        "rsync -avz --update --copy-links --progress -e 'ssh -i ./deploy_key -p %i' %s/* %s@%s:%s" %
+        (repo("ssh.port"), path("mirror"), repo("ssh.user"), repo("ssh.host"), repo("ssh.path")))
+
+    if app("is_travis") is True:
+        print(title("Deploy to git remote") + "\n")
+
+        os.system("git push https://${GITHUB_TOKEN}@%s HEAD:master" % git_remote_path())
+
+def create_database():
+    print(title("Create database") + "\n")
+
+    database = repo("database")
+    path_mirror = path("mirror")
+    os.chdir(path_mirror)
+
+    scripts = [
+        "rm -f ./%s.db" % database,
+        "rm -f ./%s.old" % database,
+        "rm -f ./%s.files" % database,
+        "rm -f ./%s.db.tar.gz" % database,
+        "rm -f ./%s.files.tar.gz" % database,
+        "rm -f ./%s.files.tar.gz.old" % database,
+        "repo-add --nocolor ./%s.db.tar.gz ./*.pkg.tar.xz" % database
+    ]
+
+    for script in scripts:
+        os.system(script)
+
+def execute(name):
+    module = package(name)
+    module.separator()
+    module.prepare()
+    module.pull()
+
+    if module.make() is True:
+        builder.append(name)
+        return True
+
+
+class package():
+    def __init__(self, name):
+        __import__(name + ".package")
+
+        self.name = name
+        self.path_pkg = path("pkg")
+        self.path_mirror = path("mirror")
+
+        self.path = self.path_pkg + "/" + self.name
+        self.package = sys.modules[self.name + ".package"]
+
+        os.chdir(self.path_pkg + "/" + self.name)
+
+    def separator(self):
+        print(title(self.name))
+
+    def prepare(self):
+        self.set_utils()
         self.clean_directory()
 
-    def clean_directory(self):
-        scripts = [
-            "rm -f ./%s.old" % self.database,
-            "rm -f ./%s.files" % self.database,
-            "rm -f ./%s.files.tar.gz" % self.database,
-            "rm -f ./%s.files.tar.gz.old" % self.database
-        ]
+    def read(self):
+        command = 'echo $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]})'
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True, executable='/bin/bash')
+        line = process.stdout.readlines()[0]
 
-        for script in scripts:
-            os.system(script)
+        return line.strip().decode("UTF-8").split(" ")
 
-    def create_database(self):
-        scripts = [
-            "rm -f ./%s.db" % self.database,
-            "rm -f ./%s.db.tar.gz" % self.database,
-            "repo-add ./%s.db.tar.gz ./*.pkg.tar.xz" % self.database
-        ]
+    def make(self):
+        self.version = extract(self.path, "pkgver")
 
-        for script in scripts:
-            os.system(script)
+        if "pre_build" in dir(self.package):
+            self.package.pre_build()
 
-    def deploy(self):
-        ssh = "ssh -p " + self.port
-        location = self.user + "@" + self.host + ":" + self.path
+        if self.has_new_version():
+            self.verify_dependencies()
+
+            if len(builder) > 0 and app("is_travis") is True:
+                return True
+
+            self.build()
+            self.commit()
+
+            return True
+
+    def commit(self):
+        print(bold("Commit changes:"))
 
         os.system(
-            "rsync -avz --update --copy-links --progress -e '%s' %s/ %s"
-            % (ssh, self.path_mirror, location)
-        )
+            "git add . && " + \
+            "git commit -m \"Bot: Add last update into " + self.package.name + " package ~ version " + self.version + "\"")
 
-        if self.is_travis():
-            os.system('git push https://${GITHUB_TOKEN}@%s HEAD:master' % git_remote_path())
+    def has_new_version(self):
+        if output("git status . --porcelain | sed s/^...//") is None:
+            return False
+
+        for f in os.listdir(self.path_mirror):
+            if f.startswith(self.name + '-' + self.version + '-'):
+                return False
+
+        return True
+
+    def verify_dependencies(self):
+        redirect = False
+        official = []
+        aur = []
+
+        for dependency in self.read():
+            try:
+                output("pacman -Ssq %s | grep '^%s$'" % (dependency, dependency))
+                official.append(dependency)
+            except:
+                aur.append(dependency)
+                pass
+
+        for name in aur:
+            if name not in self.packages:
+                sys.exit("\nError: %s is not part of the official package and can't be found in pkg directory." % name)
+
+            if name not in builder:
+                redirect = True
+                execute(name, self)
+
+        if redirect is True and app("is_travis") is False:
+            self.separator()
+
+        os.chdir(self.path_pkg + "/" + self.name)
+
+    def build(self):
+        print(bold("Build package:"))
+
+        os.system(
+            "makepkg \
+                --clean \
+                --install \
+                --nocheck \
+                --nocolor \
+                --noconfirm \
+                --skipinteg \
+                --syncdeps; " \
+            "mv *.pkg.tar.xz " + self.path_mirror);
+
+    def pull(self):
+        print(bold("Clone repository:"))
+
+        os.system(
+            "git init --quiet; "
+            "git remote add origin " + self.package.source + "; "
+            "git pull origin master; "
+            "rm -rf .git;")
+
+        if os.path.isfile(".SRCINFO"):
+            os.remove(".SRCINFO")
+
+    def clean_directory(self):
+        files = os.listdir(".")
+        for f in files:
+            if os.path.isdir(f):
+                os.system("rm -rf " + f)
+            elif os.path.isfile(f) and f != "package.py":
+                os.remove(f)
+
+    def set_utils(self):
+        self.package.edit_file = edit_file
+        self.package.replace_ending = replace_ending
