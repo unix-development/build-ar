66670667bc3cb04df9e246f6bae180024eccbd3d
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-04 20:05:01 -0500

    Utils: Add title and bold functions to terminal

--- bot/utils/terminal.py
+++ bot/utils/terminal.py
@@ -7,5 +7,12 @@ import subprocess
 def output(command):
     return subprocess.check_output(command, shell=True).decode(sys.stdout.encoding).strip()
 
-def column():
-    return int(output("tput cols")) - 4
+def title(text):
+    column = int(output("tput cols")) - 4
+    line = "── %s " % text.capitalize()
+    line += "─" * (column - len(text))
+
+    return bold(line)
+
+def bold(text):
+    return "\n\033[1m" + text + "\033[0m"
