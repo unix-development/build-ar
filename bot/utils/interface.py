b85086abab6efc36fe9685c10398ecddb808b057
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-09 01:31:19 -0500

    Utils: Optimize interface file

--- bot/utils/interface.py
+++ bot/utils/interface.py
@@ -7,7 +7,6 @@ def get_compressed_file(path):
     with open(path) as f:
         return " ".join(f.read().split())
 
-
 def get_base64(path):
     with open(path, 'rb') as f:
         return base64.b64encode(f.read()).decode('utf8')
