d76b5aaa078b1f543b3be9737c9ccb536f19ec8f
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-10 17:12:31 -0400

    Environment: Refactor file to increase readability

--- bot/environment.py
+++ bot/environment.py
@@ -5,7 +5,10 @@ import os
 import textwrap
 import subprocess
 
-from core.container import container, get, config
+from core.container import (
+    app, config, container
+)
+
 
 def register():
     (container
@@ -14,47 +17,52 @@ def register():
         .register("environment.prepare_pacman", prepare_pacman)
         .register("environment.prepare_ssh", prepare_ssh))
 
-def execute(commands):
-    subprocess.Popen(commands, stderr=subprocess.PIPE, shell=True)
-
 def prepare_mirror():
-    execute("chmod 777" + get("path.mirror"))
+    execute("chmod 777 %s" % app("path.mirror"))
 
 def prepare_git():
-    execute("""
-    git config user.email '{email}';
-    git config user.name '{name}';
-    """.format(
-        email=config("git.email"),
-        name=config("git.name")
-    ))
+    execute(
+        "git config user.email '%s'; " % config("git.email") +
+        "git config user.name '%s';" % config("git.name")
+    )
 
 def prepare_ssh():
-    execute("""
-    eval $(ssh-agent);
-    chmod 600 {base}/deploy_key;
-    ssh-add {base}/deploy_key;
-    mkdir -p ~/.ssh;
-    chmod 0700 ~/.ssh;
-    ssh-keyscan -t rsa -H {host} >> ~/.ssh/known_hosts;
-    """.format(
-        base=get("path.base"),
-        host=config("ssh.host")
-    ))
+    execute(
+        "eval $(ssh-agent); " +
+        "chmod 600 ./deploy_key; " +
+        "ssh-add ./deploy_key; " +
+        "mkdir -p ~/.ssh; " +
+        "chmod 0700 ~/.ssh; " +
+        "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; "
+        % config("ssh.host")
+    )
 
 def prepare_pacman():
-    database = config("database")
-    mirror = get("path.mirror")
+    path = "{mirror}/{database}.db".format(
+        database=config("database"),
+        mirror=app("path.mirror")
+    )
 
-    if os.path.exists(mirror + "/" + database + ".db"):
+    content = ("""
+        [{database}]
+        SigLevel = Optional TrustedOnly
+        Server = file:///{mirror}
+        """.format(
+            database=config("database"),
+            mirror=app("path.mirror")
+        )
+    )
+
+    if os.path.exists(path):
         with open("/etc/pacman.conf", "a+") as fp:
-            fp.write(textwrap.dedent("""
-            [{database}]
-            SigLevel = Optional TrustedOnly
-            Server = file:///{mirror}
-            """.format(
-                database=database,
-                mirror=mirror
-            )))
+            fp.write(textwrap.dedent(content))
 
     execute("sudo pacman -Sy")
+
+def execute(commands):
+    subprocess.run(
+        commands,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True
+    )
