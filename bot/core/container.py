ebfc6b747ba953a2e05918167cce0809d8698fb3
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-11 11:31:22 -0400

    Core: Add instances class into container file

--- bot/core/container.py
+++ bot/core/container.py
@@ -3,59 +3,58 @@
 
 import sys
 import functools
+import collections
 
 
-def return_self(name):
-    @functools.wraps(name)
-    def wrapper(*args, **kwargs):
-        self = args[0]
-        name(*args, **kwargs)
-        return self
+class Instances(collections.defaultdict):
+    def __init__(self):
+        super(Instances, self).__init__(Instances)
 
-    return wrapper
+    def __getattr__(self, abstract):
+        try:
+            return self[abstract]
+        except KeyError:
+            raise AttributeError(abstract)
 
-def app(abstract):
-    return container.get(abstract)
-
-def config(abstract):
-    value = container.get("repository")
-    for key in abstract.split('.'):
-        value = value[key]
-
-    return value
-
-def repo(abstract):
-    value = container.get("repository")
-    for key in abstract.split('.'):
-        value = value[key]
-
-    return value
-
-def text(abstract):
-    keys = abstract.split(".", 1)
-    texts = container.get("text")
-
-    return texts[keys[0]][keys[1]]
+    def __setattr__(self, abstract, value):
+        self[abstract] = value
 
 
-class Container():
-    instances = dict()
-
-    @return_self
-    def register(self, abstract, instance):
-        self.instances[abstract] = instance
+class Container(object):
+    def __init__(self):
+        self.bindings = dict()
+        self.instances = Instances()
 
     def run(self):
-        functions = self.get("runner").get()
+        functions = self.bindings["runner"].get()
         for name in functions:
-            self.get(name)()
+            self.bindings[name]()
 
-    def get(self, abstract):
-        return self.instances[abstract]
+    def register(self, abstract, value):
+        self.bindings[abstract] = value
 
     def bootstrap(self, bootstrappers):
         for bootstrap in bootstrappers:
-            bootstrap()
+            __import__(bootstrap)
+            module = sys.modules[bootstrap]
+            module.app = self.instances
+            module.config = self.instances.config
+            module.text = self._text
+            module.container = self
+            module.register()
 
+    def _text(self, abstract):
+        keys = abstract.split(".", 1)
+        return self.instances.text[keys[0]][keys[1]]
+
+
+def return_self(method):
+    @functools.wraps(method)
+    def enforced_method(*args, **kwargs):
+        self = args[0]
+        method(*args, **kwargs)
+        return self
+
+    return enforced_method
 
 container = Container()
