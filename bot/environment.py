7ebaef3ee54a749063bf528948024b9fff3fc49b
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-11 11:37:08 -0400

    Environment: Use environment class instead of functions

--- bot/environment.py
+++ bot/environment.py
@@ -5,64 +5,54 @@ import os
 import textwrap
 import subprocess
 
-from core.container import (
-    app, config, container
-)
+
+class Environment(object):
+    def prepare_mirror(self):
+        self._execute("chmod 777 " + app.mirror)
+
+    def prepare_git(self):
+        self._execute(
+            "git config user.email '%s'; " % config.git.email +
+            "git config user.name '%s'" % config.git.name
+        )
+
+    def prepare_ssh(self):
+        self._execute(
+            "eval $(ssh-agent); " +
+            "chmod 600 ./deploy_key; " +
+            "ssh-add ./deploy_key; " +
+            "mkdir -p ~/.ssh; " +
+            "chmod 0700 ~/.ssh; " +
+            "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; "
+            % config.ssh.host
+        )
+
+    def prepare_pacman(self):
+        content = (f"""
+        [{config.database}]
+        SigLevel = Optional TrustedOnly
+        Server = file:///{app.mirror}
+        """)
+
+        if os.path.exists(f"{app.mirror}/{config.database}.db"):
+            with open("/etc/pacman.conf", "a+") as fp:
+                fp.write(textwrap.dedent(content))
+
+        self._execute("sudo pacman -Sy")
+
+    def _execute(self, commands):
+        subprocess.run(
+            commands,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True
+        )
 
 
 def register():
-    (container
-        .register("environment.prepare_git", prepare_git)
-        .register("environment.prepare_mirror", prepare_mirror)
-        .register("environment.prepare_pacman", prepare_pacman)
-        .register("environment.prepare_ssh", prepare_ssh))
+    environment = Environment()
 
-def prepare_mirror():
-    execute("chmod 777 %s" % app("path.mirror"))
-
-def prepare_git():
-    execute(
-        "git config user.email '%s'; " % config("git.email") +
-        "git config user.name '%s';" % config("git.name")
-    )
-
-def prepare_ssh():
-    execute(
-        "eval $(ssh-agent); " +
-        "chmod 600 ./deploy_key; " +
-        "ssh-add ./deploy_key; " +
-        "mkdir -p ~/.ssh; " +
-        "chmod 0700 ~/.ssh; " +
-        "ssh-keyscan -t rsa -H %s >> ~/.ssh/known_hosts; "
-        % config("ssh.host")
-    )
-
-def prepare_pacman():
-    path = "{mirror}/{database}.db".format(
-        database=config("database"),
-        mirror=app("path.mirror")
-    )
-
-    content = ("""
-        [{database}]
-        SigLevel = Optional TrustedOnly
-        Server = file:///{mirror}
-        """.format(
-            database=config("database"),
-            mirror=app("path.mirror")
-        )
-    )
-
-    if os.path.exists(path):
-        with open("/etc/pacman.conf", "a+") as fp:
-            fp.write(textwrap.dedent(content))
-
-    execute("sudo pacman -Sy")
-
-def execute(commands):
-    subprocess.run(
-        commands,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        shell=True
-    )
+    container.register("environment.prepare_git", environment.prepare_git)
+    container.register("environment.prepare_mirror", environment.prepare_mirror)
+    container.register("environment.prepare_pacman", environment.prepare_pacman)
+    container.register("environment.prepare_ssh", environment.prepare_ssh)
