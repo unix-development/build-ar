5e1869b6eba8d5a42d8cda875d1205ebf6b86e0b
Marc-Antoine Loignon <developer@lognoz.org>
2019-03-13 20:04:56 -0400

    Utils: Rename execute function to strict_execute

--- bot/utils/process.py
+++ bot/utils/process.py
@@ -8,14 +8,13 @@ import subprocess
 def output(command):
     return subprocess.check_output(command, shell=True).decode(sys.stdout.encoding).strip()
 
-def execute(commands):
-    for command in commands:
-        try:
-            process = subprocess.check_call(command, shell=True)
-        except subprocess.CalledProcessError as error:
-            sys.exit(error)
-        except OSError as error:
-            sys.exit(error)
+def strict_execute(command):
+    try:
+        process = subprocess.check_call(command, shell=True)
+    except subprocess.CalledProcessError as error:
+        sys.exit(error)
+    except OSError as error:
+        sys.exit(error)
 
 def git_remote_path():
     return output('git remote get-url origin') \
