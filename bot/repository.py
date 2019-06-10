09de7f31064f73b05aa04227aeb081ea471595d4
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-10 08:45:27 -0400

    Repository: Fix bug when git failed to push some refs

--- bot/repository.py
+++ bot/repository.py
@@ -83,7 +83,12 @@ class Repository():
 
         if app.is_travis:
             print(title(text("content.repository.deploy.git")) + "\n")
-            strict_execute("git push https://%s@%s HEAD:master" % (config.github.token, git_remote_path()))
+
+            try:
+                subprocess.check_call("git push https://%s@%s HEAD:master &> /dev/null" % (
+                    config.github.token, git_remote_path()), shell=True)
+            except:
+                sys.exit("Error: Failed to push some refs to 'https://%s'" % git_remote_path())
 
     def set_package_checked(self, name):
         with open(f"{app.mirror}/packages_checked", "a+") as f:
