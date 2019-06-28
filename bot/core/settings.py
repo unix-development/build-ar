ea277fa6225fa991165d0c49107dfa1c6898ea96
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 15:39:09 -0400

    Core: Rephrase bot version comment by explaining how to get the monthly commit since the last month

--- bot/core/settings.py
+++ bot/core/settings.py
@@ -8,6 +8,8 @@ See the file 'LICENSE' for copying permission
 from core.type import Attr
 
 # Bot version (<major>.<minor>.<month>.<monthly commit>)
+# To get the monthly commit, you need to execute:
+#   git rev-list --count HEAD --since="last month"
 VERSION = "1.0.6.33"
 
 # Contextual paths
