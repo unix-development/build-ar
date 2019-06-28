71ff0efba6f2ac357df31c0486462341114bb1c7
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 19:07:42 -0400

    Core: Move data in settings.py into data.py

--- bot/core/settings.py
+++ bot/core/settings.py
@@ -5,24 +5,10 @@ Copyright (c) Build Your Own Arch Linux Repository developers
 See the file 'LICENSE' for copying permission
 """
 
-from core.type import Attr
-
-
 # Github upstream source
 GITHUB_UPSTREAM = "https://github.com/unix-development/build-your-own-archlinux-repository"
 
 # Bot version (<major>.<minor>.<month>.<monthly commit>)
-# To get the monthly commit, you need to execute:
-#
-# >>> git rev-list --count HEAD --since="last month"
-# 33
+# To get the monthly commit, you need to execute
+# git rev-list --count HEAD --since="last month"
 VERSION = "1.0.6.33"
-
-# Contextual paths
-paths = Attr()
-
-# Config into repository.json
-configs = None
-
-# Packages in pkg directory
-packages = []
