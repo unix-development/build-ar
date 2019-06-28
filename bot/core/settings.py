0db83e04d0bf7930e75d109b155d7caedd7153d9
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 19:00:44 -0400

    Core: Add upstream constant to settings.py

--- bot/core/settings.py
+++ bot/core/settings.py
@@ -7,16 +7,22 @@ See the file 'LICENSE' for copying permission
 
 from core.type import Attr
 
+
+# Github upstream source
+GITHUB_UPSTREAM = "https://github.com/unix-development/build-your-own-archlinux-repository"
+
 # Bot version (<major>.<minor>.<month>.<monthly commit>)
 # To get the monthly commit, you need to execute:
-#   git rev-list --count HEAD --since="last month"
+#
+# >>> git rev-list --count HEAD --since="last month"
+# 33
 VERSION = "1.0.6.33"
 
 # Contextual paths
 paths = Attr()
 
 # Config into repository.json
-configs = Attr()
+configs = None
 
 # Packages in pkg directory
 packages = []
