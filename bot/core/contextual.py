afcc6cb5e98464a8e3ea4b019f3d2a1cb85311eb
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 19:25:08 -0400

    Core: Change the way to register contextual elements

--- bot/core/contextual.py
+++ bot/core/contextual.py
@@ -7,101 +7,61 @@ See the file 'LICENSE' for copying permission
 
 import os
 import json
-import yaml
 
-from datetime import datetime
-from core.settings import configs
-from core.settings import packages
-from core.settings import paths
+from utils.process import is_travis
+from core.data import conf
+from core.data import repository
+from core.data import paths
+from core.type import Dict
 
 
-class Contextual(object):
-    def set_contextual_paths(self):
-        root = self._get_base_path()
+def set_contextual_paths(root):
+    paths.base = root
+    paths.mirror = os.path.join(root, "mirror")
+    paths.pkg = os.path.join(root, "pkg")
+    paths.www = os.path.join(root, "bot/www")
 
-        paths.base = root
-        paths.mirror = os.path.join(root, "mirror")
-        paths.pkg = os.path.join(root, "pkg")
-        paths.www = os.path.join(root, "bot/www")
+def base_path():
+    return os.path.realpath(__file__).replace("/bot/core/contextual.py", "")
 
-    def set_packages(self):
-        for name in os.listdir(paths.pkg):
-            if os.path.isfile(os.path.join(paths.pkg, name, "package.py")):
-                packages.append(name)
+def set_repository():
+    matches = []
+    for name in os.listdir(paths.pkg):
+        if os.path.isfile(os.path.join(paths.pkg, name, "package.py")):
+            matches.append(name)
 
-        packages.sort()
+    matches.sort()
 
-        if app.is_travis is True:
-            packages = self._get_packages_sorted(packages)
+    if is_travis() is True:
+        matches = get_sorted_packages(matches)
 
-    def set_repository(self):
-        path = os.path.join(paths.base, "repository.json")
-        if os.path.isfile(path):
-            with open(path) as fp:
-                configs = Attr(json.load(fp))
+    repository.extend(matches)
 
-    def set_texts(self):
-        app.text = dict(
-            exception = self._get_text("exception"),
-            content = self._get_text("content")
-        )
+def get_sorted_packages(matches):
+    path = os.path.join(paths.mirror, "packages_checked")
 
-    def _get_text(self, abstract):
-        path = f"{app.base}/bot/text/{abstract}.yml"
+    if not os.path.exists(path):
+        os.mknod(path)
 
-        with open(path, "r") as fp:
-            return yaml.load(fp)
+    with open(path) as fp:
+        checked = fp.read().splitlines()
 
-    def _get_packages_sorted(self, packages):
-        path = f"{app.mirror}/packages_checked"
-        if not os.path.exists(path):
-            os.mknod(path)
-        else:
-            today = datetime.now()
-            last_modification = datetime.fromtimestamp(
-                os.path.getctime(path))
+    not_checked = list(set(matches) - set(checked))
+    if len(not_checked) == 0:
+        with open(path, 'w'):
+            pass
+    else:
+        not_checked.sort()
 
-            if today.date() > last_modification.date():
-                with open(path, "w"):
-                    pass
+    return (not_checked + checked)
 
+def set_configs():
+    path = os.path.join(paths.base, "repository.json")
+    if os.path.isfile(path):
         with open(path) as fp:
-            packages_checked_today = list(
-                set(fp.read().splitlines())
-            )
-
-        packages_tmp = list(
-            set(packages) - set(packages_checked_today)
-        )
-
-        if len(packages_tmp) == 0:
-            with open(path, 'w'):
-                pass
-
-            return packages
-        else:
-            packages_tmp.sort()
-            return (packages_tmp + packages_checked_today)
-
-    def _get_value(self, abstract, repository):
-        value = repository
-        for name in abstract.split('.'):
-            if name in value:
-                value = value[name]
-
-        if value != repository:
-            return repository
-        else:
-            return ""
-
-    def _get_base_path(self):
-        return os.path.realpath(__file__).replace("/bot/core/contextual.py", "")
-
+            conf.user = Dict(json.load(fp))
 
 def register():
-    contextual = Contextual()
-
-    contextual.set_contextual_paths()
-    contextual.set_packages()
-    contextual.set_repository()
-    contextual.set_texts()
+    set_contextual_paths(base_path())
+    set_repository()
+    set_configs()
