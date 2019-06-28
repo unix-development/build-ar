d9941b42bc2b9e5944094022aa57ed5dadd04f05
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 15:16:28 -0400

    Core: Add initial settings.py

---
+++ bot/core/settings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) Build Your Own Arch Linux Repository developers
+See the file 'LICENSE' for copying permission
+"""
+
+from core.type import Attr
+
+
+# Contextual paths
+paths = Attr()
+
+# Config into repository.json
+configs = Attr()
+
+# Packages in pkg directory
+packages = []
