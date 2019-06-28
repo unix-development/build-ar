3c64f2a1ad72800f9cdb84bfb8c9f54d333e4325
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 15:09:07 -0400

    Core: Remove instance class into container.py

--- bot/core/container.py
+++ bot/core/container.py
@@ -7,27 +7,11 @@ See the file 'LICENSE' for copying permission
 
 import sys
 import functools
-import collections
-
-
-class Instances(collections.defaultdict):
-    def __init__(self):
-        super(Instances, self).__init__(Instances)
-
-    def __getattr__(self, abstract):
-        try:
-            return self[abstract]
-        except KeyError:
-            raise AttributeError(abstract)
-
-    def __setattr__(self, abstract, value):
-        self[abstract] = value
 
 
 class Container(object):
     def __init__(self):
         self.bindings = dict()
-        self.instances = Instances()
 
     def run(self):
         functions = self.bindings["runner"].get()
@@ -41,10 +25,10 @@ class Container(object):
         for bootstrap in bootstrappers:
             __import__(bootstrap)
             module = sys.modules[bootstrap]
-            module.app = self.instances
-            module.config = self.instances.config
-            module.text = self._text
-            module.container = self
+            # module.app = self.instances
+            # module.config = self.instances.config
+            # module.text = self._text
+            # module.container = self
             module.register()
 
     def _text(self, abstract):
@@ -60,5 +44,6 @@ def return_self(method):
         return self
 
     return enforced_method
+
 
 container = Container()
