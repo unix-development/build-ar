d066cb5c497aad23c48cb63d5848f006f0c3ae61
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 15:34:29 -0400

    Core: Add bot version

--- bot/core/settings.py
+++ bot/core/settings.py
@@ -7,6 +7,8 @@ See the file 'LICENSE' for copying permission
 
 from core.type import Attr
 
+# Bot version (<major>.<minor>.<month>.<monthly commit>)
+VERSION = "1.0.6.33"
 
 # Contextual paths
 paths = Attr()
