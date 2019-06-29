53c30c6d0328f72acfe96fe94477b21d8273c999
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-28 23:53:19 -0400

    Validator: Optimize the way to test modules

--- bot/validator.py
+++ bot/validator.py
@@ -7,17 +7,15 @@ See the file 'LICENSE' for copying permission
 
 import os
 import yaml
-import sys
 import json
 import socket
 import secrets
 import platform
 import requests
 
-from core.container import return_self
-from core.container import container
 from core.data import paths
 from core.data import conf
+from core.data import repository
 from core.type import get_attr_value
 from utils.process import git_remote_path
 from utils.process import is_travis
@@ -93,7 +91,7 @@ def check_repository():
     )
 
 def check_content():
-    repository = {}
+    configs = {}
     expected = [
         "url",
         "database",
@@ -104,11 +102,11 @@ def check_content():
     ]
 
     for name in expected:
-        repository[name] = get_attr_value(conf.user, name)
+        configs[name] = get_attr_value(conf.user, name)
 
     valid = True
-    for name in repository:
-        if not repository[name]:
+    for name in configs:
+        if not configs[name]:
             valid = False
             break
 
@@ -132,139 +130,130 @@ def check_port():
         valid=type(conf.user.ssh.port) == int
     )
 
+def check_travis_lint(content):
+    validate(
+        error="An error occured while trying to parse your travis file.\nPlease make sure that the file is valid YAML.",
+        target="lint",
+        valid=type(content) is dict
+    )
 
-class Validator():
+def check_travis_openssl(content):
+    valid = False
 
-    @return_self
-    def ssh_connection(self):
-        script = "ssh -i ./deploy_key -p %i -q %s@%s [[ -d %s ]] && echo 1 || echo 0" % (
-            config.ssh.port,
-            config.ssh.user,
-            config.ssh.host,
-            config.ssh.path
-        )
+    if "before_install" in content:
+        for statement in content["before_install"]:
+            if statement.startswith("openssl"):
+                valid = True
 
-        validate(
-            error=text("exception.validator.ssh.connection"),
-            target=text("content.validator.ssh.connection"),
-            valid=output(script) is "1"
-        )
+    validate(
+        error="No openssl statement could be found in your travis file.\nPlease make sure to execute: travis encrypt-file ./deploy_key --add",
+        target="openssl",
+        valid=valid
+    )
 
-    @return_self
-    def mirror_connection(self):
-        ssh = config.ssh
-        token = secrets.token_hex(15)
-        source = f"{app.mirror}/validation_token"
+def check_pkg_directory():
+    validate(
+        error="No package was found in pkg directory.",
+        target="directory",
+        valid=len(repository) > 0
+    )
 
-        with open(source, "w") as f:
-            f.write(token)
-            f.close()
+def check_pkg_content():
+    folders = [f.name for f in os.scandir(paths.pkg) if f.is_dir()]
+    diff = set(folders) - set(repository)
 
-        os.system("rsync -aqvz -e 'ssh -i ./deploy_key -p %i' %s %s@%s:%s" % (
-            ssh.port, source, ssh.user, ssh.host, ssh.path)
-        )
+    validate(
+        error="No package.py was found in pkg subdirectories: %s" % (", ".join(diff)),
+        target="content",
+        valid=len(diff) == 0
+    )
 
-        try:
-            response = requests.get(config.url + "/validation_token")
-            valid = True if response.status_code == 200 else False
-        except:
-            valid = False
+# @TO TEST
+def check_pkg_testing():
+    try:
+        conf.testing.environment
+    except AttributeError:
+        return
 
-        validate(
-            error=text("exception.validator.mirror.connection") % config.url,
-            target=text("content.validator.mirror.connection"),
-            valid=valid
-        )
+    valid = True
+    error = ""
 
-    @return_self
-    def github_token(self):
+    if conf.testing.package is None:
         valid = False
-        user = git_remote_path().split("/")[1]
-        response = output(f"curl -su {user}:{config.github.token} https://api.github.com/user")
-        content = json.loads(response)
+        error = "You need to define which package you want to test with this command: make package test=discord"
 
-        if "login" in content:
-            valid = True
+    elif conf.testing.package not in conf.packages:
+        valid = False
+        error = "%s is not in pkg directory." % conf.testing.package
 
-        validate(
-            error=text("exception.validator.travis.github.token"),
-            target=text("content.validator.travis.github.token"),
-            valid=valid
-        )
+    validate(
+        error=error,
+        target="testing",
+        valid=valid
+    )
 
-    @return_self
-    def travis_lint(self, content):
-        validate(
-            error=text("exception.validator.travis.lint"),
-            target=text("content.validator.travis.lint"),
-            valid=type(content) is dict
-        )
+def check_ssh_connection():
+    script = "ssh -i ./deploy_key -p %i -q %s@%s [[ -d %s ]] && echo 1 || echo 0" % (
+        conf.user.ssh.port,
+        conf.user.ssh.user,
+        conf.user.ssh.host,
+        conf.user.ssh.path
+    )
 
-    @return_self
-    def travis_openssl(self, content):
+    validate(
+        error="ssh connection could not be established.",
+        target="ssh address",
+        valid=output(script) is "1"
+    )
+
+def check_mirror_connection():
+    ssh = conf.user.ssh
+    token = secrets.token_hex(15)
+    source = os.path.join(paths.mirror, "validation_token")
+
+    with open(source, "w") as f:
+        f.write(token)
+        f.close()
+
+    os.system("rsync -aqvz -e 'ssh -i ./deploy_key -p %i' %s %s@%s:%s" % (
+        ssh.port, source, ssh.user, ssh.host, ssh.path)
+    )
+
+    try:
+        response = requests.get(conf.user.url + "/validation_token")
+        valid = True if response.status_code == 200 else False
+    except:
         valid = False
 
-        if "before_install" in content:
-            for statement in content["before_install"]:
-                if statement.startswith("openssl"):
-                    valid = True
+    validate(
+        error="This program can't connect to %s." % conf.user.url,
+        target="mirror host",
+        valid=valid
+    )
 
-        validate(
-            error=text("exception.validator.travis.openssl"),
-            target=text("content.validator.travis.openssl"),
-            valid=valid
-        )
+def check_github_token():
+    valid = False
+    user = git_remote_path().split("/")[1]
+    response = output("curl -su %s:%s https://api.github.com/user" % (user, conf.user.github.token))
+    content = json.loads(response)
 
-    @return_self
-    def pkg_directory(self):
-        validate(
-            error=text("exception.validator.pkg.directory"),
-            target=text("content.validator.pkg.directory"),
-            valid=len(app.packages) > 0
-        )
-
-    @return_self
-    def pkg_content(self):
-        folders = [f.name for f in os.scandir(app.pkg) if f.is_dir()]
-        diff = set(folders) - set(app.packages)
-
-        validate(
-            error=text("exception.validator.pkg.content") % (", ".join(diff)),
-            target=text("content.validator.pkg.content"),
-            valid=len(diff) == 0
-        )
-
-    @return_self
-    def pkg_testing(self):
-        if app.testing.environment is not True:
-            return
-
+    if "login" in content:
         valid = True
-        error = ""
 
-        if app.testing.package is None:
-            valid = False
-            error = text("exception.validator.pkg.testing.argument")
-
-        elif app.testing.package not in app.packages:
-            valid = False
-            error = text("exception.validator.pkg.testing.package") % app.testing.package
-
-        validate(
-            error=error,
-            target=text("content.validator.pkg.testing"),
-            valid=valid
-        )
-
+    validate(
+        error="An error occured while trying to connect to your github repository with your encrypted token.\nPlease make sure that your token is working.",
+        target="github token api",
+        valid=valid
+    )
 
 def register():
     return {
         "validator.requirements": requirements,
         "validator.files": files,
-        "validator.repository": repository,
-        #"validator.content": content,
-        #"validator.travis": travis,
-        #"validator.connection": connection,
+        "validator.configs": configs,
+        "validator.travis": travis,
+        "validator.content": content,
+        "validator.connection": connection
     }
 
 def requirements():
@@ -281,44 +270,38 @@ def files():
     check_repository()
     check_deploy_key()
 
-def repository():
+def configs():
     print("Validating repository:")
 
     check_content()
     check_database()
     check_port()
 
-#def connection():
-#    print("Validating connection:")
-#
-#    (validator
-#        .ssh_connection()
-#        .mirror_connection()
-#        .github_token())
-#
-#def content():
-#    print("Validating packages:")
-#
-#    (validator
-#        .pkg_directory()
-#        .pkg_content()
-#        .pkg_testing())
-#
-#def travis():
-#    if is_travis() is False:
-#        return
-#
-#    print("Validating travis:")
-#
-#    with open(".travis.yml", "r") as stream:
-#        try:
-#            content = yaml.load(stream)
-#        except yaml.YAMLError as error:
-#            content = error
-#
-#    (validator
-#        .travis_lint(content)
-#        .travis_openssl(content))
+def connection():
+    print("Validating connection:")
 
+    check_ssh_connection()
+    check_mirror_connection()
+    check_github_token()
 
-validator = Validator()
+def content():
+    print("Validating packages:")
+
+    check_pkg_directory()
+    check_pkg_content()
+    check_pkg_testing()
+
+def travis():
+    if is_travis() is False:
+        return
+
+    print("Validating travis:")
+
+    with open(".travis.yml", "r") as stream:
+        try:
+            content = yaml.load(stream)
+        except yaml.YAMLError as error:
+            content = error
+
+    check_travis_lint(content)
+    check_travis_openssl(content)
