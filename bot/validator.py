55445d6d8d6237c777cc9748e81362a3aa8fb844
Marc-Antoine Loignon <developer@lognoz.org>
2019-06-30 14:08:03 -0400

    Validator: Optimize functions and change double quotes to single one

--- bot/validator.py
+++ bot/validator.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 
 """
@@ -25,55 +26,55 @@ from utils.validator import validate
 
 def _check_user_privileges():
     validate(
-        error="This program needs to be not execute as root.",
-        target="user privileges",
-        valid=os.getuid() != 0
+        error='This program needs to be not execute as root.',
+        target='user privileges',
+        valid=(os.getuid() != 0)
     )
 
 
 def _check_is_docker_image():
     validate(
-        error="This program needs to be executed in a docker image.",
-        target="docker",
-        valid=os.environ.get("IS_DOCKER", False)
+        error='This program needs to be executed in a docker image.',
+        target='docker',
+        valid=(os.environ.get('IS_DOCKER', False))
     )
 
 
 def _check_operating_system():
     validate(
-        error="This program needs to be executed in Arch Linux.",
-        target="operating system",
-        valid=(platform.dist()[0] == "arch")
+        error='This program needs to be executed in Arch Linux.',
+        target='operating system',
+        valid=(platform.dist()[0] == 'arch')
     )
 
 
 def _check_internet_up():
     try:
-        socket.create_connection(("www.github.com", 80))
+        socket.create_connection(('www.github.com', 80))
         connected = True
     except OSError:
         connected = False
 
     validate(
-        error="This program needs to be connected to internet.",
-        target="internet",
+        error='This program needs to be connected to internet.',
+        target='internet',
         valid=connected
     )
 
 
 def _check_deploy_key():
     valid = True
-    target = "deploy_key"
+    target = 'deploy_key'
 
-    if IS_TRAVIS and os.path.isfile(os.path.join(paths.base, "deploy_key.enc")) is False:
+    if IS_TRAVIS and os.path.isfile(os.path.join(paths.base, 'deploy_key.enc')) is False:
         valid = False
-        target = "deploy_key.enc"
+        target = 'deploy_key.enc'
 
-    elif os.path.isfile(os.path.join(paths.base, "deploy_key")) is False:
+    elif os.path.isfile(os.path.join(paths.base, 'deploy_key')) is False:
         valid = False
 
     validate(
-        error="%s could not been found." % target,
+        error='%s could not been found.' % target,
         target=target,
         valid=valid
     )
@@ -81,17 +82,17 @@ def _check_deploy_key():
 
 def _check_repository():
     valid = True
-    target = "repository.json"
+    target = 'repository.json'
 
-    if IS_TRAVIS and os.path.isfile(os.path.join(paths.base, "repository.json.enc")) is False:
+    if IS_TRAVIS and os.path.isfile(os.path.join(paths.base, 'repository.json.enc')) is False:
         valid = False
-        target = "repository.json.enc"
+        target = 'repository.json.enc'
 
-    elif os.path.isfile(os.path.join(paths.base, "repository.json")) is False:
+    elif os.path.isfile(os.path.join(paths.base, 'repository.json')) is False:
         valid = False
 
     validate(
-        error="%s could not been found." % target,
+        error='%s could not been found.' % target,
         target=target,
         valid=valid
     )
@@ -100,12 +101,12 @@ def _check_repository():
 def _check_content():
     configs = {}
     expected = [
-        "url",
-        "database",
-        "github token",
-        "ssh host",
-        "ssh path",
-        "ssh port"
+        'url',
+        'database',
+        'github token',
+        'ssh host',
+        'ssh path',
+        'ssh port'
     ]
 
     for name in expected:
@@ -118,55 +119,62 @@ def _check_content():
             break
 
     validate(
-        error="%s must be defined in repository.json" % name,
-        target="content",
+        error='%s must be defined in repository.json' % name,
+        target='content',
         valid=valid
     )
 
 
 def _check_database():
     validate(
-        error="Database must be different than core, community and extra.",
-        target="database",
-        valid=conf.user.database not in ["core", "extra", "community"]
+        error='Database must be different than core, community and extra.',
+        target='database',
+        valid=conf.user.database not in ('core', 'extra', 'community')
     )
 
 
 def _check_port():
     validate(
-        error="port must be an interger in repository.json",
-        target="port",
-        valid=type(conf.user.ssh.port) == int
+        error='port must be an interger in repository.json',
+        target='port',
+        valid=(type(conf.user.ssh.port) == int)
     )
 
 
 def _check_travis_lint(content):
+    error_msg = 'An error occured while trying to parse your travis file.\n'
+    error_msg += 'Please make sure that the file is valid YAML.',
+
     validate(
-        error="An error occured while trying to parse your travis file.\nPlease make sure that the file is valid YAML.",
-        target="lint",
-        valid=type(content) is dict
+        error=error_msg,
+        target='lint',
+        valid=(type(content) is dict)
     )
 
 
 def _check_travis_openssl(content):
     valid = False
 
-    if "before_install" in content:
-        for statement in content["before_install"]:
-            if statement.startswith("openssl"):
+    error_msg = 'No openssl statement could be found in your travis file.\n'
+    error_msg += 'Please make sure to execute: '
+    error_msg += 'travis encrypt-file ./deploy_key --add',
+
+    if 'before_install' in content:
+        for statement in content['before_install']:
+            if statement.startswith('openssl'):
                 valid = True
 
     validate(
-        error="No openssl statement could be found in your travis file.\nPlease make sure to execute: travis encrypt-file ./deploy_key --add",
-        target="openssl",
+        error=error_msg,
+        target='openssl',
         valid=valid
     )
 
 
 def _check_pkg_directory():
     validate(
-        error="No package was found in pkg directory.",
-        target="directory",
+        error='No package was found in pkg directory.',
+        target='directory',
         valid=len(repository) > 0
     )
 
@@ -176,8 +184,8 @@ def _check_pkg_content():
     diff = set(folders) - set(repository)
 
     validate(
-        error="No package.py was found in pkg subdirectories: %s" % (", ".join(diff)),
-        target="content",
+        error='No package.py was found in pkg subdirectories: %s' % (', '.join(diff)),
+        target='content',
         valid=len(diff) == 0
     )
 
@@ -187,29 +195,29 @@ def _check_pkg_testing():
         return
 
     valid = True
-    error = ""
+    error_msg = ''
 
     if conf.testing.package is None:
         valid = False
-        error = "You need to define which package you want to test with this command: make package test=discord"
+        error_msg = 'You need to define which package you want to test with this command: make package test=discord'
 
     elif conf.testing.package not in repository:
         valid = False
-        error = "%s is not in pkg directory." % conf.testing.package
+        error_msg = '%s is not in pkg directory.' % conf.testing.package
 
-    elif output("git status " + paths.pkg + "/" + conf.testing.package + " --porcelain | sed s/^...//"):
+    elif output('git status ' + paths.pkg + '/' + conf.testing.package + ' --porcelain | sed s/^...//'):
         valid = False
-        error = "You need to commit your changes before to test your package."
+        error_msg = 'You need to commit your changes before to test your package.'
 
     validate(
-        error=error,
-        target="testing",
+        error=error_msg,
+        target='testing',
         valid=valid
     )
 
 
 def _check_ssh_connection():
-    script = "ssh -i ./deploy_key -p %i -q %s@%s [[ -d %s ]] && echo 1 || echo 0" % (
+    script = 'ssh -i ./deploy_key -p %i -q %s@%s [[ -d %s ]] && echo 1 || echo 0' % (
         conf.user.ssh.port,
         conf.user.ssh.user,
         conf.user.ssh.host,
@@ -217,57 +225,60 @@ def _check_ssh_connection():
     )
 
     validate(
-        error="ssh connection could not be established.",
-        target="ssh address",
-        valid=output(script) is "1"
+        error='ssh connection could not be established.',
+        target='ssh address',
+        valid=output(script) is '1'
     )
 
 
 def _check_mirror_connection():
     ssh = conf.user.ssh
     token = secrets.token_hex(15)
-    source = os.path.join(paths.mirror, "validation_token")
+    source = os.path.join(paths.mirror, 'validation_token')
 
-    with open(source, "w") as f:
+    with open(source, 'w') as f:
         f.write(token)
         f.close()
 
-    os.system("rsync -aqvz -e 'ssh -i ./deploy_key -p %i' %s %s@%s:%s" % (
+    os.system('rsync -aqvz -e "ssh -i ./deploy_key -p %i" %s %s@%s:%s' % (
         ssh.port, source, ssh.user, ssh.host, ssh.path)
     )
 
     try:
-        response = requests.get(conf.user.url + "/validation_token")
+        response = requests.get(conf.user.url + '/validation_token')
         valid = True if response.status_code == 200 else False
-    except:
+    except requests.RequestException:
         valid = False
 
     validate(
-        error="This program can't connect to %s." % conf.user.url,
-        target="mirror host",
+        error='This program can\'t connect to %s.' % conf.user.url,
+        target='mirror host',
         valid=valid
     )
 
 
 def _check_github_token():
     valid = False
-    user = git_remote_path().split("/")[1]
-    response = output("curl -su %s:%s https://api.github.com/user" % (user, conf.user.github.token))
+    user = git_remote_path().split('/')[1]
+    response = output('curl -su %s:%s https://api.github.com/user' % (user, conf.user.github.token))
     content = json.loads(response)
 
-    if "login" in content:
+    if 'login' in content:
         valid = True
 
+    error_msg = 'An error occured while trying to connect to your github repository with your encrypted token.\n'
+    error_msg += 'Please make sure that your token is working.'
+
     validate(
-        error="An error occured while trying to connect to your github repository with your encrypted token.\nPlease make sure that your token is working.",
-        target="github token api",
+        error=error_msg,
+        target='github token api',
         valid=valid
     )
 
 
 class Validator():
     def requirements(self):
-        print("Validating requirements:")
+        print('Validating requirements:')
 
         _check_user_privileges()
         _check_is_docker_image()
@@ -275,27 +286,27 @@ class Validator():
         _check_internet_up()
 
     def files(self):
-        print("Validating files:")
+        print('Validating files:')
 
         _check_repository()
         _check_deploy_key()
 
     def configs(self):
-        print("Validating repository:")
+        print('Validating repository:')
 
         _check_content()
         _check_database()
         _check_port()
 
     def connection(self):
-        print("Validating connection:")
+        print('Validating connection:')
 
         _check_ssh_connection()
         _check_mirror_connection()
         _check_github_token()
 
     def content(self):
-        print("Validating packages:")
+        print('Validating packages:')
 
         _check_pkg_directory()
         _check_pkg_content()
@@ -305,9 +316,9 @@ class Validator():
         if IS_TRAVIS is False:
             return
 
-        print("Validating travis:")
+        print('Validating travis:')
 
-        with open(".travis.yml", "r") as stream:
+        with open('.travis.yml', 'r') as stream:
             try:
                 content = yaml.load(stream)
             except yaml.YAMLError as error:
