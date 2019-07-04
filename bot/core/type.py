0cee33d95aacdea8150063d0b5953c79f9a4cb75
Marc-Antoine Loignon <developer@lognoz.org>
2019-07-04 12:52:17 -0400

    Core: Drop useless class into type.py

--- bot/core/type.py
+++ bot/core/type.py
@@ -5,47 +5,60 @@ Copyright (c) Build Your Own Arch Linux Repository developers
 See the file 'LICENSE' for copying permission
 """
 
-import collections
 
-
-class Attr(collections.defaultdict):
+class Attr(dict):
     """
-    This class defines an object, inheriting from Python data
-    type dictionary.
+    This class defines the dictionary with added capability to access members as attributes
 
-    >>> foo = Attr()
+    >>> foo = AttribDict()
     >>> foo.bar = 1
     >>> foo.bar
     1
     """
-    def __init__(self):
-        super(Attr, self).__init__(Attr)
 
-    def __getattr__(self, abstract):
+    def __init__(self, indict=None, attribute=None):
+        if indict is None:
+            indict = {}
+
+        # Set any attributes here - before initialisation
+        # these remain as normal attributes
+        self.attribute = attribute
+        dict.__init__(self, indict)
+        self.__initialised = True
+
+    def __getattr__(self, item):
+        """
+        Maps values to attributes
+        Only called if there *is NOT* an attribute with this name
+        """
+
         try:
-            return self[abstract]
+            return self.__getitem__(item)
         except KeyError:
-            raise AttributeError(abstract)
+            raise AttributeError("unable to access item '%s'" % item)
 
-    def __setattr__(self, abstract, value):
-        self[abstract] = value
+    def __setattr__(self, item, value):
+        """
+        Maps attributes to values
+        Only if we are initialised
+        """
 
+        # This test allows attributes to be set in the __init__ method
+        if "_AttribDict__initialised" not in self.__dict__:
+            return dict.__setattr__(self, item, value)
 
-class Dict(dict):
-    def __init__(self, arg):
-        super(Dict, self).__init__(arg)
+        # Any normal attributes are handled normally
+        elif item in self.__dict__:
+            dict.__setattr__(self, item, value)
 
-        for key, value in arg.items():
-            if type(value) is dict:
-                self[key] = Dict(value)
-            else:
-                self[key] = value
+        else:
+            self.__setitem__(item, value)
 
-    def __getattr__(self, attr):
-        return self.get(attr)
+    def __getstate__(self):
+        return self.__dict__
 
-    def __setattr__(self, key, value):
-        self.__setitem__(key, value)
+    def __setstate__(self, dict):
+        self.__dict__ = dict
 
 
 def get_attr_value(ref, name):
@@ -56,4 +69,3 @@ def get_attr_value(ref, name):
             return None
 
     return ref
-
