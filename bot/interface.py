3e2a8d088c75ec389c8a1b39a07ad10134f59fb0
Marc-Antoine Loignon <developer@lognoz.org>
2019-07-08 22:48:36 -0400

    Interface: Add markdown readme builder

--- bot/interface.py
+++ bot/interface.py
@@ -12,14 +12,15 @@ from datetime import datetime
 from core.data import conf
 from core.data import paths
 from utils.editor import edit_file
-from utils.process import git_remote_path
-from utils.process import output
 from utils.process import extract
-
+from utils.process import git_remote_path
+from utils.process import has_git_changes
+from utils.process import output
+from utils.process import strict_execute
 
 class Interface():
-    table = ""
-    content = """
+    html_table_tbody = ""
+    html_table_tr = """
     <tr>
         <td><a href="$path">$name</a></td>
         <td>$version</td>
@@ -27,6 +28,9 @@ class Interface():
         <td>$description</td>
     </tr>
     """
+
+    markdown_table_tbody = ""
+    markdown_table_tr = "*$name*<br>$description | $version | $date\n"
 
     def create(self):
         conf.packages.sort()
@@ -45,27 +49,52 @@ class Interface():
             description = schema["description"]
 
             for name in schema["name"].split(" "):
-                build = self.get_package_file(name, schema)
+                path = self.get_package_file(name, schema)
 
-                if build:
+                if path:
                     description = self.get_description(package, name, description)
 
-                    self.table += (self.content
-                        .replace("$path", build)
-                        .replace("$name", name)
-                        .replace("$date", date)
-                        .replace("$version", version)
-                        .replace("$description", description)
-                    )
+                    for prefix in ["html", "markdown"]:
+                        tr = getattr(self, prefix + "_table_tr")
+                        tr = tr.replace("$path", path)
+                        tr = tr.replace("$name", name)
+                        tr = tr.replace("$date", date)
+                        tr = tr.replace("$version", version)
 
+                        if prefix is "markdown":
+                            description = description.replace("\\", "\\\\")
+                            description = description.replace("*", "\*")
+                            description = description.replace("_", "\_")
+                            description = description.replace("|", "\|")
+
+                        tr = tr.replace("$description", description)
+
+                        tbody = getattr(self, prefix + "_table_tbody")
+                        setattr(self, prefix + "_table_tbody", tbody + tr)
+
+        # Create html mirror
         self.move_to_mirror()
-        self.replace_variables()
+        self.replace_html_variables()
         self.compress()
+
+        # Creade README.md
+        self.move_to_root()
+        self.replace_markdown_variables()
+        self.commit_readme()
 
     def get_last_change(self, path):
         last_change = output("git log -1 --format='%at' -- " + path)
         timestamp = datetime.fromtimestamp(int(last_change))
         return timestamp.strftime("%d %h %Y")
+
+    def commit_readme(self):
+        path = paths.base + "/README.md"
+
+        if (has_git_changes(path)):
+            strict_execute(f"""
+            git add {path};
+            git commit -m "Doc: Add recent changes into packages information table";
+            """)
 
     def get_schema(self, path):
         if not os.path.isfile(path + "/PKGBUILD"):
@@ -105,19 +134,30 @@ class Interface():
         return description
 
     def move_to_mirror(self):
-        os.system(f"cp {paths.www}/index.html {paths.mirror}")
+        os.system(f"cp {paths.www}/template.html {paths.mirror}/index.html")
 
-    def replace_variables(self):
+    def move_to_root(self):
+        os.system(f"cp {paths.www}/template.md {paths.base}/README.md")
+
+    def replace_markdown_variables(self):
+        for line in edit_file(paths.base + "/README.md"):
+            line = line.replace("$content", self.markdown_table_tbody)
+            line = line.replace("$path", conf.url)
+            line = line.replace("$database_capitalize", conf.db.capitalize())
+            line = line.replace("$database", conf.db)
+
+            print(line)
+
+    def replace_html_variables(self):
         remote_path = 'https://' + git_remote_path().rstrip('.git')
 
         for line in edit_file(paths.mirror + "/index.html"):
-            line = (line
-                .replace("$content", self.table)
-                .replace("$path", conf.url)
-                .replace("$database", conf.db)
-                .replace("$remote_path", remote_path)
-                .replace("images/logo.png", "data:image/png;base64," + get_base64(paths.www + "/images/logo.png"))
-                .replace("images/background.png", "data:image/png;base64," + get_base64(paths.www + "/images/background.png")))
+            line = line.replace("$content", self.html_table_tbody)
+            line = line.replace("$path", conf.url)
+            line = line.replace("$database", conf.db)
+            line = line.replace("$remote_path", remote_path)
+            line = line.replace("images/logo.png", "data:image/png;base64," + get_base64(paths.www + "/images/logo.png"))
+            line = line.replace("images/background.png", "data:image/png;base64," + get_base64(paths.www + "/images/background.png"))
 
             if line.strip() == "<link rel=\"stylesheet\" href=\"css/main.css\">":
                 line = "<style type=\"text/css\">"
