e0b68f68796e0499618b5c6972d259e02b339089
Marc-Antoine Loignon <developer@lognoz.org>
2019-07-21 13:30:58 -0400

    Utils: Add copy_file function

--- bot/utils/editor.py
+++ bot/utils/editor.py
@@ -5,15 +5,27 @@ Copyright (c) Build Your Own Arch Linux Repository developers
 See the file 'LICENSE' for copying permission
 """
 
+import os
+import sys
 import fileinput
+
+from shutil import copy2
 
 
 def replace_ending(find, replace, string):
     split = string.split(find, 1)
     return split[0] + replace
 
-
 def edit_file(filename):
     with fileinput.input(filename, inplace=1) as f:
         for line in f:
             yield line.rstrip('\n')
+
+def copy_file(src, dest=""):
+    src = src.strip("/")
+    dest = dest.strip("/")
+
+    src = os.path.join(sys.path[1], "../files", src)
+    dest = os.path.join(sys.path[0], dest)
+
+    copy2(src, dest)
