e694628e82ab3a8fca2c77f3f127ce580d4cf352
Marc-Antoine Loignon <developer@lognoz.org>
2020-03-03 11:41:51 -0500

    Environment: Add function to prepare git repository and mirror remote

--- src/environment.py
+++ src/environment.py
@@ -5,14 +5,22 @@ Copyright (c) Build Your Own Arch Linux Repository developers
 See the file 'LICENSE' for copying permission
 """
 
+import os
+import time
+import sys
+import subprocess
 
 from core.app import app
 
 from util.process import execute
+from util.process import output
 
 
 class Environment():
     def prepare_ssh(self):
+        """
+        This function prepare ssh before to interact with the remote.
+        """
         execute(f"""
         eval $(ssh-agent);
         chmod 600 ./deploy_key;
@@ -32,6 +40,46 @@ class Environment():
                 IdentityFile {app.path.base}/deploy_key
             """)
             f.close()
+
+    def prepare_git(self):
+        """
+        This function prepare git by setting user name and email.
+        Uvobot is used to commit repository changes.
+        """
+        execute("""
+        git config --global user.email 'uvobot@lognoz.org';
+        git config --global user.name 'uvobot';
+        """)
+
+    def prepare_mirror(self):
+        """
+        This function mirror by verifing if we can pull files online.
+        """
+        black_list   = [ "validation_token", "packages_checked" ]
+        staged       = output("git ls-files " + app.path.mirror).strip()
+        in_directory = os.listdir(app.path.mirror)
+
+        if staged != "":
+            black_list = staged.split("\n") + black_list
+
+        for f in black_list:
+            if f in in_directory:
+                in_directory.remove(f)
+
+        if not app.has("ssh") or in_directory != []:
+            return
+
+        print("Updating local mirror directory... ")
+
+        command = (f"""
+        rsync \
+            --update \
+            --progress \
+            {app.ssh.user}@{app.ssh.host}:{app.ssh.path}/libc* \
+            {app.path.mirror}/
+        """)
+
+        os.system(command)
 
 
 environment = Environment()
